b23cfee50b4ecbc81058289cf033fbe5c9c1c498
aman <amannegi229@gmail.com>
2022-09-11 13:21:21 +0530

    Initial Commit

---
+++ linkedin/linkedinApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkedinApp.swift
+//  linkedin
+//
+//  Created by Aman Negi on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct linkedinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
