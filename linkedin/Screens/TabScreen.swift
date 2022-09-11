39011e54a0c3b6d8870dae08abb4362ed777045c
aman <amannegi229@gmail.com>
2022-09-11 16:01:37 +0530

    implemented bottom tab bar

---
+++ linkedin/Screens/TabScreen.swift
@@ -0,0 +1,46 @@
+//
+//  TabScreen.swift
+//  linkedin
+//
+//  Created by Aman Negi on 11/09/22.
+//
+
+import SwiftUI
+
+struct TabScreen: View {
+    var body: some View {
+        TabView {
+            HomeScreen()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            MyNetworkScreen()
+                .tabItem {
+                    Image(systemName: "person.2.fill")
+                    Text("My Network")
+                }
+            Text("Post")
+                .tabItem {
+                    Image(systemName: "plus.app.fill")
+                    Text("Post")
+                }
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "bell.badge.fill")
+                    Text("Notifications")
+                }
+            Text("Jobs")
+                .tabItem {
+                    Image(systemName: "briefcase.fill")
+                    Text("Jobs")
+                }
+        }
+    }
+}
+
+struct TabScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        TabScreen()
+    }
+}
